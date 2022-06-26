5a5118b689bce07ab9364ea2ccf952433656df93
Eziz <38165397+webdeveziz@users.noreply.github.com>
2022-06-26 15:50:25 +0300

    17
    17

---
+++ src/app/components/ui/comments.jsx
@@ -0,0 +1,39 @@
+import { orderBy } from "lodash";
+import React from "react";
+import CommentsList, { AddCommentForm } from "../common/comments";
+import { useComments } from "../../hooks/useComments";
+
+const Comments = () => {
+    const { createComment, comments, removeComment } = useComments();
+
+    const handleSubmit = (data) => {
+        createComment(data);
+    };
+    const handleRemoveComment = (id) => {
+        removeComment(id);
+    };
+    const sortedComments = orderBy(comments, ["created_at"], ["desc"]);
+    return (
+        <>
+            <div className="card mb-2">
+                <div className="card-body ">
+                    <AddCommentForm onSubmit={handleSubmit} />
+                </div>
+            </div>
+            {sortedComments.length > 0 && (
+                <div className="card mb-3">
+                    <div className="card-body ">
+                        <h2>Comments</h2>
+                        <hr />
+                        <CommentsList
+                            comments={sortedComments}
+                            onRemove={handleRemoveComment}
+                        />
+                    </div>
+                </div>
+            )}
+        </>
+    );
+};
+
+export default Comments;
